c03e6855b8ecd0d303ce6d2c8f44d5ee5d79b876
jarbasai <jarbasai@mailfence.com>
2018-02-22 19:49:10 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='jurebes',
+    version='0.1',
+    packages=['jurebes', 'jurebes.tts', 'jurebes.intent', 'jurebes.listener',
+              'jurebes.messagebus'],
+    url='https://github.com/JarbasAl/jurebes',
+    license='MIT',
+    author='jarbasAi',
+    author_email='jarbasai@mailfence.com',
+    description='mycroft dev tools'
+)
