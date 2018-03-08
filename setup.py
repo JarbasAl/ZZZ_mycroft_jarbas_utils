2aeafce07b896b21982f0d51d5b148f3a149d743
jarbasai <jarbasai@mailfence.com>
2018-03-08 20:25:05 +0000

    auto_tx

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from distutils.core import setup
 setup(
     name='mycroft_jarbas_utils',
     version='0.1',
-    packages=['mycroft_jarbas_utils', 'mycroft_jarbas_utils.tts',
-              'mycroft_jarbas_utils.intent', 'mycroft_jarbas_utils.listener',
+    packages=['mycroft_jarbas_utils',
+              'mycroft_jarbas_utils.intent',
+              'mycroft_jarbas_utils.browser',
               'mycroft_jarbas_utils.messagebus',
               'mycroft_jarbas_utils.skills'],
     install_requires=[
