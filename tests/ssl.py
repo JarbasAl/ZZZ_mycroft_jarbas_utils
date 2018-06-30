b613e1b45b08521e24b205e5b53b69d60ddd0d51
jarbasal <jarbasai@mailfence.com>
2018-06-30 19:01:18 +0100

    unittests

--- tests/ssl.py
+++ tests/ssl.py
@@ -1,18 +1,18 @@
 import unittest
 from os.path import exists, join, expanduser
-from os import makedirs
+from os import remove
 from mycroft_jarbas_utils.ssl import create_self_signed_cert
 
 
 class TestSSL(unittest.TestCase):
     def test_self_signed(self):
         name = "self_signed_jarbas"
-        path = join(expanduser("~"), ".jarbas")
-        if not exists(path):
-            makedirs(path)
+        path = expanduser("~")
         create_self_signed_cert(path, name)
         assert exists(join(path, name + ".crt"))
+        remove(join(path, name + ".crt"))
         assert exists(join(path, name + ".key"))
+        remove(join(path, name + ".key"))
 
 
 if __name__ == "__main__":
